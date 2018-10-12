575196bcdfb6b0a49eb8830434a760c43f7306a8
yixin <39300550+1261762904@users.noreply.github.com>
2018-10-12 20:44:39 +0800

    Update Select.java

--- orm/src/orm/anno/Select.java
+++ orm/src/orm/anno/Select.java
@@ -5,12 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 标记要执行 SELECT 语句的方法
- * 
- * @author wtao
- *
- */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Select {
