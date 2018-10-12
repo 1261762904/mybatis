8c06a050a976b3d289e904088fcb52c7fd21ca93
yixin <39300550+1261762904@users.noreply.github.com>
2018-10-12 20:44:20 +0800

    Update Insert.java

--- orm/src/orm/anno/Insert.java
+++ orm/src/orm/anno/Insert.java
@@ -5,12 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 标记执行 INSERT 语句的方法
- * 
- * @author wtao
- *
- */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Insert {
